1b88e8814b15886066c14192d4e9212fed8d39db
Hjcom <tkddnrgks3@gmail.com>
2019-02-27 21:01:12 +0900

    Early models.py

--- app/eduGalaxy/models.py
+++ app/eduGalaxy/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class EduGalaxyUser(models.Model):
+
+    user_email = models.EmailField(max_length=50)
+    user_pwd = models.CharField(max_length=15)
+    user_nickname = models.CharField(max_length=15)
+    user_age = models.IntegerField()
+    user_job = models.CharField()
+    user_sex = models.BooleanField()
+    user_address1 = models.CharField(max_length=100)
+    user_address2 = models.CharField(max_length=100)
+    user_phone = models.CharField(max_length=15)
+    # user_post = models.ForeignKey('', on_delete=models.CASCADE)
+    user_receive_email = models.BooleanField()
+    user_confirm = models.BooleanField()
+    user_created_date = models.DateTimeField(default=timezone.now)
+    user_signup_ip = models.CharField(max_length=100)
+
+    # published_date = models.DateTimeField(blank=True, null=True)
+    #
+    # def publish(self):
+    #     self.published_date = timezone.now()
+    #     self.save()
+    #
+    def __str__(self):
+        return self.user_email + ' ' + self.user_nickname
+
