89bb3a6c02065b8c6d3ab6b502d13c3b13bed065
jhlee-Ubuntu <lejung92@gmail.com>
2019-02-27 22:05:56 +0900

    Add templates/* and eduGalaxy/urls.py

---
+++ app/eduGalaxy/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+#app_name = 'eduGalaxy'
+urlpatterns = [
+    # the 'name' value as called by the {% url %} template tag
+    path('', views.index, name='index'),
+]
