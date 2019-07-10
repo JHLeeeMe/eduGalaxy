9a82ddc68376e27d5d2b4c634402f4be58928453
Hjcom <tkddnrgks3@gmail.com>
2019-07-10 16:37:16 +0900

    admin csv import

--- eduGalaxy/apps/school/admin.py
+++ eduGalaxy/apps/school/admin.py
@@ -5,14 +5,10 @@ from django.contrib import admin
 import csv
 
 
-class CsvImportForm(forms.ModelForm):
+class CsvImportForm(forms.Form):
     csv_file = forms.FileField()
 
-    class Meta:
-        model = SchoolInfo
-        fields = ("csv_file",)
-
-    def save(self, commit=False, *args, **kwargs):
+    def save(self):
         school = SchoolInfo()
 
         file_csv = self.cleaned_data['csv_file']
@@ -20,40 +16,40 @@ class CsvImportForm(forms.ModelForm):
         records = csv.reader(datafile)
 
         for line in records:
-            school.sch_ooe = line[1]
-            school.sch_lea = line[2]
-            school.sch_location = line[3]
+            school.sch_ooe = line[0]
+            school.sch_lea = line[1]
+            school.sch_location = line[2]
 
-            school.sch_inf_code = line[4]
-            school.sch_name = line[5]
-            school.sch_grade_code= line[6]
+            school.sch_inf_code = line[3]
+            school.sch_name = line[4]
+            school.sch_grade_code= line[5]
 
-            school.sch_estab_div = line[7]
-            school.sch_char = line[8]
-            school.sch_has_branches = line[9]
+            school.sch_estab_div = line[6]
+            school.sch_char = line[7]
+            school.sch_has_branches = line[8]
 
-            school.sch_estab_type = line[10]
-            school.sch_day_n_night = line[11]
-            school.sch_anniversary = line[12]
+            school.sch_estab_type = line[9]
+            school.sch_day_n_night = line[10]
+            school.sch_anniversary = line[11]
 
-            school.sch_estab_date = line[10]
-            school.sch_dong_code = line[11]
-            school.sch_address1 = line[12]
+            school.sch_estab_date = line[12]
+            school.sch_dong_code = line[13]
+            school.sch_address1 = line[14]
 
-            school.sch_address2 = line[13]
-            school.sch_zip_code = line[14]
-            school.sch_zip_code_st = line[15]
+            school.sch_address2 = line[15]
+            school.sch_zip_code = line[16]
+            school.sch_zip_code_st = line[17]
 
-            school.sch_address1_st = line[16]
-            school.sch_address2_st = line[17]
-            school.sch_lat = line[18]
+            school.sch_address1_st = line[18]
+            school.sch_address2_st = line[19]
+            school.sch_lat = line[20]
 
-            school.sch_lng = line[16]
-            school.sch_phone = line[17]
-            school.sch_fax = line[18]
+            school.sch_lng = line[21]
+            school.sch_phone = line[22]
+            school.sch_fax = line[23]
 
-            school.sch_homepage = line[16]
-            school.sch_gonghak = line[17]
+            school.sch_homepage = line[24]
+            school.sch_gonghak = line[25]
 
             school.save()
 
@@ -61,7 +57,10 @@ class CsvImportForm(forms.ModelForm):
 
 
 class SchoolInfoAdmin(admin.ModelAdmin):
-    form = CsvImportForm()
+    change_list_template = "admin/schoolInfo_changelist.html"
+
+    def get_urls(self):
+
 
 
 admin.site.register(SchoolInfo, SchoolInfoAdmin)
