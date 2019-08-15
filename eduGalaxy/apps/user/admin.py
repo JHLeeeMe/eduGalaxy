c2ef19c2aafa6970fd7d26f428af61ad8013efb5
Hjcom <tkddnrgks3@gmail.com>
2019-08-15 20:18:10 +0900

    user/admin.py 1차

--- eduGalaxy/apps/user/admin.py
+++ eduGalaxy/apps/user/admin.py
@@ -1,90 +1,213 @@
-# from django import forms
-# from django.contrib import admin
-# from django.contrib.auth.models import Group
-# from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-# from django.contrib.auth.forms import ReadOnlyPasswordHashField
-#
-# from .models import EduUser
-#
-#
-# class EduUserCreationForm(forms.ModelForm):
-#     password1 = forms.CharField(label='비밀번호', widget=forms.PasswordInput)
-#     password2 = forms.CharField(label='비밀번호 확인', widget=forms.PasswordInput)
-#
-#     class Meta:
-#         model = EduUser
-#         fields = ('user_email', 'user_nickname')
-#
-#     def clean_password2(self):
-#         password1 = self.cleaned_data.get("password1")
-#         password2 = self.cleaned_data.get("password2")
-#
-#         if password1 and password2 and password1 != password2:
-#             raise forms.ValidationError("비밀번호 오류입니다.")
-#         return password2
-#
-#     def save(self, commit=True):
-#         user = super().save(commit=False)
-#         user.set_password(self.cleaned_data["password1"])
-#
-#         if commit:
-#             user.save()
-#         return user
-#
-#
-# class EduUserChangeForm(forms.ModelForm):
-#
-#     password = ReadOnlyPasswordHashField()
-#
-#     class Meta:
-#         model = EduUser
-#         fields = ('user_email', 'password', 'user_nickname', 'is_active', 'is_admin')
-#
-#     def clean_password(self):
-#         return self.initial['password']
-#
-#
-# class EduUserAdmin(BaseUserAdmin):
-#     form = EduUserChangeForm
-#     add_form = EduUserCreationForm
-#
-#     list_display =(
-#         'id',
-#         'user_email',
-#         'password',
-#         'user_nickname',
-#         'user_age',
-#         'user_job',
-#         'user_gender',
-#         'user_address1',
-#         'user_address2',
-#         'user_phone',
-#         'last_login',
-#         'user_receive_email',
-#         'user_confirm',
-#         'user_signup_ip',
-#         'date_joined',
-#         'is_active',
-#         'is_admin')
-#     list_filter = ('is_admin',)
-#     fieldsets = (
-#         (None, {'fields': ('user_email', 'password','user_nickname')}),
-#         ('관리자 여부', {'fields': ('is_admin',)}),
-#     )
-#
-#     add_fieldsets = (
-#         (None, {
-#             'classes': ('wide',),
-#             'fields': ('user_email', 'user_nickname', 'password1', 'password2')}
-#          ),
-#     )
-#
-#     search_fields = ('user_email',)
-#     ordering = ('date_joined',)
-#     filter_horizontal = ()
-#
-#
-# admin.site.register(EduUser, EduUserAdmin)
-# admin.site.unregister(Group)
-#
-#
+from django import forms
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+from apps.user.models import EdUser, Profile, SchoolAuth, Student
+from apps.user.models import Parent, Child, Log
+
+
+class EdUserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='비밀번호', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='비밀번호 확인', widget=forms.PasswordInput)
+
+    class Meta:
+        model = EdUser
+        fields = ('email', 'nickname')
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("비밀번호 오류입니다.")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+
+        if commit:
+            user.save()
+        return user
+
+
+class EdUserChangeForm(forms.ModelForm):
+
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = EdUser
+        fields = ('email', 'password', 'nickname', 'is_active', 'is_admin')
+
+    def clean_password(self):
+        return self.initial['password']
+
+
+class EdUserAdmin(BaseUserAdmin):
+    form = EdUserChangeForm
+    add_form = EdUserCreationForm
+
+    list_display =(
+        'id',
+        'email',
+        'nickname',
+        'is_active',
+        'is_admin')
+    list_filter = ('is_admin', 'is_active')
+    list_per_page = 15
+    list_display_links = ('email', )
+    ordering = ('id', )
+    fieldsets = (
+        (None, {'fields': ('email', 'nickname')}),
+        ('계정 관리', {'fields': ('is_admin', 'is_active')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'nickname', 'password1', 'password2')}
+         ),
+    )
+
+    search_fields = ('email',)
+    filter_horizontal = ()
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('get_id',
+                    'get_email',
+                    'group',
+                    'receive_email',
+                    'confirm')
+    list_display_links = ('get_email',)
+    list_filter = ('group', )
+
+    list_per_page = 15
+
+    def get_id(self, obj):
+        return obj.eduser.id
+
+    def get_email(self, obj):
+        return obj.eduser.email
+
+    get_id.short_description = 'ID'
+    get_email.short_description = '이메일'
+    get_id.admin_order_field = 'eduser'
+
+
+class SchoolAuthAdmin(admin.ModelAdmin):
+    list_display = ('get_id',
+                    'get_email',
+                    'school',
+                    'tel',
+                    'get_confirm')
+    list_display_links = ('get_email',)
+    list_per_page = 15
+
+    def get_id(self, obj):
+        return obj.profile.eduser.id
+
+    def get_email(self, obj):
+        return obj.profile.eduser.email
+
+    def get_confirm(self, obj):
+        return obj.profile.confirm
+
+    get_id.short_description = 'ID'
+    get_email.short_description = '이메일'
+    get_confirm.short_description = '인증 여부'
+    get_id.admin_order_field = 'profile'
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('get_id',
+                    'get_email',
+                    'school',
+                    'age')
+    list_display_links = ('get_email',)
+
+    list_per_page = 15
+
+    def get_id(self, obj):
+        return obj.profile.eduser.id
+
+    def get_email(self, obj):
+        return obj.profile.eduser.email
+
+    get_id.short_description = 'ID'
+    get_email.short_description = '이메일'
+    get_id.admin_order_field = 'profile'
+
+
+class ParentAdmin(admin.ModelAdmin):
+    list_display = ('get_id',
+                    'get_email',
+                    'address1')
+    list_display_links = ('get_email',)
+
+    list_per_page = 15
+
+    def get_id(self, obj):
+        return obj.profile.eduser.id
+
+    def get_email(self, obj):
+        return obj.profile.eduser.email
+
+    get_id.short_description = 'ID'
+    get_email.short_description = '이메일'
+    get_id.admin_order_field = 'profile'
+
+
+class ChildAdmin(admin.ModelAdmin):
+    list_display = ('id',
+                    'get_email',
+                    'school',
+                    'age')
+    list_display_links = ('get_email',)
+    ordering = ('id', )
+    list_per_page = 15
+
+    def get_email(self, obj):
+        return obj.parent.profile.eduser.email
+
+    get_email.short_description = '이메일'
+
+
+class LogAdmin(admin.ModelAdmin):
+    list_display = ('get_id',
+                    'get_email',
+                    'signup_ip',
+                    'created_date',
+                    'login_ip',
+                    'get_login_date')
+    list_display_links = ('get_email',)
+
+    list_per_page = 15
+
+    def get_id(self, obj):
+        return obj.eduser.id
+
+    def get_email(self, obj):
+        return obj.eduser.email
+
+    def get_login_date(self, obj):
+        return obj.eduser.last_login
+
+    get_id.short_description = 'ID'
+    get_email.short_description = '이메일'
+    get_id.admin_order_field = 'eduser'
+    get_login_date.short_description = '마지막 로그인 날짜'
+
+
+admin.site.register(EdUser, EdUserAdmin)
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(SchoolAuth, SchoolAuthAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Parent, ParentAdmin)
+admin.site.register(Child, ChildAdmin)
+admin.site.register(Log, LogAdmin)
+admin.site.unregister(Group)
+
+
