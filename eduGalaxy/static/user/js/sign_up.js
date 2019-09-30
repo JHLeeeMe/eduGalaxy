5e5391cbedf708aad0a5fdb6ab762deee25486e9
JHLeeeMe-msi <lejung92@gmail.com>
2019-09-30 17:29:31 +0900

    sign_up.js & create_student.html 코드정리
    	sign_up.js: 학력추가 최대 6개로 제한 둠.
    	create_student.html: 코드정리

--- eduGalaxy/static/user/js/sign_up.js
+++ eduGalaxy/static/user/js/sign_up.js
@@ -25,14 +25,23 @@ function Change_Email(){
 // options[아이디.selectedIndex].text : select 박스 옵션에 맞는 텍스트
 // options[아이디.selectedIndex].value : 옵션에 맞는 값
 
+var cnt = 6
 function addField() {
-    var items = document.createElement('p');
-    items.innerHTML = document.getElementById('p_graduation').innerHTML;
-    document.getElementById('div_graduation').appendChild(items);
+    if (cnt > 0) {
+        var items = document.createElement('p');
+        items.innerHTML = document.getElementById('p_graduation').innerHTML;
+        document.getElementById('div_graduation').appendChild(items);
+        cnt -= 1
+    } else {
+        alert('더는 추가할 수 없습니다.')
+    }
 }
 
 function removeField(obj) {
-    document.getElementById('div_graduation').removeChild(obj.parentNode);
+    if (cnt < 6) {
+        document.getElementById('div_graduation').removeChild(obj.parentNode);
+        cnt += 1
+    }
 }
 
 function address_search() {
